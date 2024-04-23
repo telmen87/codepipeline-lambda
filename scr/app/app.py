65257979e5c7a9d167fe638a6b6c08c10cbdf911
telmen87 <telmen87@gmail.com>
2024-04-23 12:22:20 +0800

    Add buildspec.yml

--- scr/app/app.py
+++ scr/app/app.py
@@ -3,9 +3,8 @@ from functions import get_df
 
 def lambda_handler(event, context):
     
-    print(get_df())
 
     return {
         'statusCode' : 200,
-        'body' : f'Hello World ! {time.time()}',
+        'body' : f'Hello World ! {time.time(), get_df()}',
     }
